f69bf5eab1c05a74a026852b0e6ba52ec42d9f93
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-04-14 12:37:10 +0300

    add base "new arrivals" component

---
+++ src/components/NewArrivals.tsx
@@ -0,0 +1,65 @@
+import { FC, useCallback, useEffect, useState } from "react";
+import classes from "./NewArrivals.module.css";
+import { useAppSelector } from "../store/hooks";
+import { useDispatch } from "react-redux";
+import { fetchNewArrivals } from "../store/actions/newArrivals";
+import { Link } from "react-router-dom";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/all";
+
+const NewArrivals: FC = () => {
+  const products = useAppSelector((state) => state.newArrivals.items);
+  const dispatch = useDispatch();
+  const [index, setIndex] = useState(0);
+
+  const loadData = useCallback(async () => {
+    await dispatch(fetchNewArrivals());
+  }, [dispatch]);
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  const chunkSize = 6;
+  const chunks = [];
+  for (let i = 0; i < products.length; i += chunkSize) {
+    chunks.push(products.slice(i, i + chunkSize));
+  }
+
+  return (
+    <div className={classes.container}>
+      <h2>New Arrivals</h2>
+      <p className={classes["sub-header"]}>
+        Check out our latest arrivals for the upcoming season
+      </p>
+      <Link to={"/"} className={classes["collection-link"]}>
+        See the collection here
+      </Link>
+      <div className={classes.products}>
+        {chunks.length > 0 &&
+          chunks[index].map((item) => (
+            <div className={classes.product} key={item.id}>
+              <div>{parseInt((item.rating || "").toString())}</div>
+              <img
+                className={classes["product-image"]}
+                alt={item.name}
+                src={item.imageUrl}
+              />
+              <div className={classes.favorite}>
+                {item.isFavorite ? (
+                  <AiFillHeart style={{ color: "red" }} />
+                ) : (
+                  <AiOutlineHeart />
+                )}
+              </div>
+              <div className={classes["product-name"]}>{item.name}</div>
+              <div className={classes["product-price"]}>
+                ${item.price / 100}
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default NewArrivals;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@ import { FC } from "react";
 import Slider from "../components/slider";
 import VisualMenu from "../components/VisualMenu";
 import classes from "./index.module.css";
+import NewArrivals from "../components/NewArrivals";
 
 const Index: FC = () => {
   return (
     <div className={classes.container}>
       <Slider />
       <VisualMenu />
+      <NewArrivals />
     </div>
   );
 };
